908ed91c7e3d0ee100b38335cf141739fba03d14
Yatsekon <yannickleduff813@gmail.com>
2023-08-05 05:47:59 +0200

    initPorject
    https://youtu.be/OihoBD5ay38

---
+++ types/app.js
@@ -0,0 +1,16 @@
+const config = {
+  width: 500,
+  height: 300,
+  type: Phaser.AUTO
+}
+var game = new Phaser.Game(config);
+
+function preload(){
+  this.image.load('')
+}
+function Create(){
+
+}
+function Update(){
+
+}
