6c1c82766716e6b5f25adb34722949f1e5085a77
Yatsekon <yannickleduff813@gmail.com>
2023-08-14 06:48:48 +0200

    memoryfin

--- types/app.js
+++ types/app.js
@@ -12,6 +12,7 @@ var config = {
 var _game = new Phaser.Game(config);
 var _totalCardNumber = 8;
 var _halfCardsNumber = _totalCardNumber / 2;
+var colomnNumber = 2;
 let _currentCardInParty = [];
 var _cardToAttribute = [];
 let _cardInDouble = [];
@@ -19,7 +20,22 @@ var _numberOfDouble = 0;
 var _numberOfRandom = 0;
 var _spriteCards = [];
 let _cards = [];
+var _cardMiddle = [];
+var _cardMin = [];
+var _cardRemaining = _totalCardNumber;
+var _currentCardNumber;
+var _lastIndex = -1;
 
+var _letCardReveal = false;
+var _isCardCorrespond = false;
+var _firstCard = null;
+var _currentCard = null;
+var _firstCardNumber;
+var timedEvent;
+var _cantClick = false;
+var _playerTurn = true;
+var _startBot = false;
+let allSprites;
 function preload ()
 {
 
@@ -60,7 +76,7 @@ function create ()
 var _x = -0.35;
 var _y = -0.2;
 
-for( var i = 0; i < _totalCardNumber/2 ;i++)
+for( var i = 0; i < _totalCardNumber/colomnNumber ;i++)
 {
   var _randomSprite = Phaser.Utils.Array.GetRandom(_cards);
   _currentCardInParty.push(_randomSprite);
@@ -80,35 +96,180 @@ _y = -1.35;
 _x = -0.35;
 _halfCardsNumber = _totalCardNumber / 2;
 }
-DetermineCards();
+    DetermineCards();
 }
+
+var _margin = 52;
+var _spaceBetweenCards = 33;
+var _cardLength = 200 - 52;
+for(var i = 0; i < _totalCardNumber / colomnNumber; i++)
+{
+
+  var _minXCard = _margin + i * _cardLength + i * _spaceBetweenCards;
+  var _maxXCard = _minXCard + _cardLength
+  _cardMin.push(_minXCard);
+  // DetermineMiddleCards(_minXCard, _maxXCard);
+}
+
 for(var i = 0; i < _spriteCards.length; i++)
 {
-  totaux = _spriteCards[i].on('pointerdown', function (pointer)
-  {
-    var touchX = pointer.x;
-   var touchY = pointer.y;
 
-   var levelindex = 0;
- for(var t = 0; t <= touchX; t += touchX / _halfCardsNumber)
+    _spriteCards[i].on('pointerdown', function (pointer)
+     {
+if(_playerTurn == true)
 {
-  levelindex++;
-  console.log(levelindex);
+  var touchX = pointer.x;
+  var touchY = pointer.y;
+  var levelindex = 0;
+
+
+  levelindex = RoundTheCardNumber(touchX);
+
+
+if(touchY >= 300)
+{
+levelindex += _totalCardNumber / colomnNumber;
 }
-if(touchY == -1.35)
-levelindex += _halfCardsNumber;
 
-     this.setTexture(_cardToAttribute[levelindex], 0);
-  });
+if(levelindex >= _cardToAttribute.length)
+levelindex = _cardToAttribute.length - 1;
+if(_cantClick == false && levelindex != _lastIndex)
+{
+ _currentCard = this.setTexture(_cardToAttribute[levelindex], 0);
+
+ _currentCardNumber = _cardToAttribute[levelindex];
+ _lastIndex = levelindex;
+ VerifyIfCardCorrespond(_currentCardNumber);
+ }
+
 }
 
-console.log(_cardToAttribute);
+           });
+        }
 
+ allSprites = this.children.list.filter(x => x instanceof Phaser.GameObjects.GameObject);
 }
 function update ()
 {
-  ReturnTheCorrectCard();
+  if(_letCardReveal)
+  {
+    timedEvent = this.time.addEvent({ delay: 1000, callback: RevealCards});
+_letCardReveal = !_letCardReveal;
+  }
+
+  if(_playerTurn == false && _startBot == true)
+  {
+    timedEvent = this.time.addEvent({ delay: 1000, callback: BotPlay});
+_startBot = false;
+  }
 }
+
+
+
+
+function BotPlay()
+{
+  var _randomCardReveal = Phaser.Math.Between(0, _cardRemaining);
+  var _randomCardPicked = Phaser.Math.Between(0, _cardRemaining);
+
+for(var i = 0; i <= 2; i++)
+{
+  _spriteCards[_randomCardPicked]
+}
+_currentCard = _cardToAttribute[_randomCardReveal];
+  _spriteCards[_randomCardPicked].GameObject.Components.Texture.get(_currentCard);
+
+// var _currentCardNumber = _cardToAttribute[_randomCardReveal];
+//
+// if(_firstCard == null)
+// {
+//   _firstCard = _currentCard;
+//   _firstCardNumber = _currentCardNumber;
+//   _startBot = true;
+//   return;
+// }
+//   VerifyIfCardCorrespond(_currentCardNumber);
+ }
+
+
+
+function RevealCards()
+{
+  if(_isCardCorrespond)
+  {
+    _firstCard.setTexture(null, 0);
+    _currentCard.setTexture(null, 0);
+  }
+  else {
+    _firstCard.setTexture('back', 0);
+    _currentCard.setTexture('back', 0);
+    // _playerTurn = !_playerTurn;
+    // if(!_playerTurn)
+    // _startBot = true;
+  }
+  _firstCard = null;
+
+_cantClick = false;
+}
+function DetermineMiddleCards(min, max)
+{
+    return _cardMiddle.push((min + max) / 2);
+}
+
+
+
+
+function VerifyIfCardCorrespond(_currentCardNumber)
+{
+
+  if(_firstCard == null)
+  {
+    _firstCard = _currentCard;
+    _firstCardNumber = _currentCardNumber;
+    return;
+  }
+
+  else {
+
+    if(_firstCardNumber == _currentCardNumber)
+    {
+;
+    _isCardCorrespond = true;
+    }
+
+    else {
+
+      _isCardCorrespond = false;
+
+    }
+    _letCardReveal = true;
+    _cantClick = true;
+  }
+
+}
+
+
+
+
+function RoundTheCardNumber(PointerXValue)
+{
+  for(var i = 0; i < _cardMin.length; i++)
+  {
+    if(_cardMin[i+1] != null)
+    {
+      if(_cardMin[i] < PointerXValue && _cardMin[i + 1] > PointerXValue)
+      {
+        return i;
+      }
+    }
+    else {
+      return i;
+    }
+  }
+}
+
+
+
 
 function DetermineCards()
 {
@@ -143,16 +304,4 @@ function DetermineCards()
   _cardInDouble.push(_randomCurrentSprite);
     _numberOfRandom++;
   }
-
-}
-
-function ReturnTheCorrectCard()
-{
-
-//this.input.on('pointerdown', () => console.log('click'));
-VerifyIfCardCorrespond();
-}
-function VerifyIfCardCorrespond()
-{
-
 }
